5c0d0360744130deea6fa00b378b5def4c841403
kaiskhaldoun <kais@Kaiss-MacBook-Pro.local>
2024-07-16 15:35:20 +0100

    update index.js file to include root route and welcome message

--- app/index.js
+++ app/index.js
@@ -11,6 +11,11 @@ const SHOP_NAME = process.env.SHOP_NAME;
 const SHOPIFY_API_VERSION = process.env.SHOPIFY_API_VERSION;
 const PORT = process.env.PORT || 3000;
 
+// Root URL handler
+app.get('/', (req, res) => {
+  res.send('Welcome to Kaiszn CloudCommerce App');
+});
+
 app.get('/products', async (req, res) => {
   try {
     const response = await axios.get(`https://${SHOP_NAME}.myshopify.com/admin/api/${SHOPIFY_API_VERSION}/products.json`, {
